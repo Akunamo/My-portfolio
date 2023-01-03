226b1977e48b399c4440255bf4df768276649e42
Aviral <2knightsoldier@gmail.com>
2023-01-03 23:29:20 +0530

    cant clean too much trash remember to commit clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     NavbarComponent,
     ProjectsComponent,
     SkillsComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
