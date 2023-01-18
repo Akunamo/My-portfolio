3790e9e5f385f599755526f3e3e1447df9b19b66
Aviral <2knightsoldier@gmail.com>
2023-01-18 12:55:47 +0530

    added themeChange function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewChildren} from '@angular/core';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements AfterViewInit{
+@ViewChild('hero') header:HeroSectionComponent;
+
   title = 'Portfolio';
+
+  public lightTheme= false;
+
+  themeSwitch(event:any) {
+    this.lightTheme = !this.lightTheme;
+    this.header.changeFill()
+  }
+  ngAfterViewInit(): void {
+    
+  }
+
 }
